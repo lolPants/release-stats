1ec27127adeb79767621b12db083909a5a593ed4
Jack Baron <jackmbaron@gmail.com>
2020-11-17 14:40:40 +0000

    fix lint failures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Release Stats</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const App = () => (
+  <div>
+    <Head>
+      <title>Release Stats</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
 
-      <h1>WIP</h1>
-    </div>
-  )
-}
+    <h1>WIP</h1>
+  </div>
+)
+
+export default App
