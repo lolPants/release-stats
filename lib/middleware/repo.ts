812a76121e2c4b207c571d351986afceb3eb054f
Jack Baron <jackmbaron@gmail.com>
2020-11-17 15:30:21 +0000

    refactor repo fetcher rename user --> owner extract to exported function

--- lib/middleware/repo.ts
+++ lib/middleware/repo.ts
@@ -1,16 +1,15 @@
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import { NextApiRequest } from 'next'
 import { resolveQuery as rq } from '~utils/query'
 import { Middleware } from './middleware'
 
 export const repo: Middleware = async (request, resp, next) => {
-  const user = rq(request.query.user)
-  const repo = rq(request.query.repo)
-
-  if (user === undefined || repo === undefined) {
+  const { owner, repo } = getRepo(request)
+  if (owner === undefined || repo === undefined) {
     return resp.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST)
   }
 
-  const r = await fetch(`https://api.github.com/repos/${user}/${repo}`)
+  const r = await fetch(`https://api.github.com/repos/${owner}/${repo}`)
   if (r.status === StatusCodes.OK) return next()
   if (r.status === StatusCodes.NOT_FOUND) {
     return resp.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND)
@@ -20,3 +19,8 @@ export const repo: Middleware = async (request, resp, next) => {
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send(ReasonPhrases.INTERNAL_SERVER_ERROR)
 }
+
+export const getRepo = (request: NextApiRequest) => ({
+  owner: rq(request.query.owner),
+  repo: rq(request.query.repo),
+})
